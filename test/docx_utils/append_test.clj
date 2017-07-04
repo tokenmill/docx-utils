07e0111b289e6a88071fffd3264880034e30d879
Dainius Jocas <dainius.jocas@tokenmill.lt>
2017-07-04 13:43:18 +0300

    moved appen tests to separate namespace

---
+++ test/docx_utils/append_test.clj
@@ -0,0 +1,32 @@
+(ns docx-utils.append-test
+  (:require [clojure.test :refer :all]
+            [clojure.java.shell :refer [sh]]
+            [clojure.java.io :as io]
+            [docx-utils.core :as docx]
+            [docx-utils.schema :refer [transformation]]))
+
+
+(deftest append-test
+  (testing "Testing if transformation appends 1 Paragraph to the template document."
+    (let [output-file-path (docx/transform [(transformation :append-text "appended text")])]
+      (sh "timeout" "5s" "libreoffice" "--norestore" output-file-path)))
+
+  (testing "Testing if transformation appends 1 Paragraph to the template document."
+    (let [output-file-path (docx/transform [{:type :append-text :replacement "appended text as a map"}])]
+      (sh "timeout" "5s" "libreoffice" "--norestore" output-file-path)))
+
+  (testing "Testing if transformation appends 1 image to the template document."
+    (let [output-file-path (docx/transform [{:type :append-image :replacement (.getPath (io/resource "test-image.jpg"))}])]
+      (sh "timeout" "5s" "libreoffice" "--norestore" output-file-path)))
+
+  (testing "Testing if transformation appends 1 table to the template document."
+    (let [output-file-path (docx/transform [{:type :append-table :replacement [["cell 11" "cal 12" "cell 13"] ["cell 21" "cell 22" "cell 23"]]}])]
+      (sh "timeout" "5s" "libreoffice" "--norestore" output-file-path)))
+
+  (testing "Testing if transformation appends 1 bullet list to the template document."
+    (let [output-file-path (docx/transform [{:type :append-bullet-list :replacement ["item a" "item b" "item c"]}])]
+      (sh "timeout" "5s" "libreoffice" "--norestore" output-file-path)))
+
+  (testing "Testing if transformation appends 1 numbered list to the template document."
+    (let [output-file-path (docx/transform [{:type :append-numbered-list :replacement ["item 1" "item 2" "item 3"]}])]
+      (sh "timeout" "5s" "libreoffice" "--norestore" output-file-path))))
--- test/docx_utils/core_test.clj
+++ test/docx_utils/core_test.clj
@@ -20,28 +20,4 @@
       (is (.exists (io/as-file output-file-path))))
 
     ;; When template file doesn't exits then exception
-    (is (thrown? Exception (docx/transform "DOES-NOT-EXIST.docx" nil))))
-
-  (testing "Testing if transformation appends 1 Paragraph to the template document."
-    (let [output-file-path (docx/transform [(transformation :append-text "appended text")])]
-      (sh "timeout" "5s" "libreoffice" "--norestore" output-file-path)))
-
-  (testing "Testing if transformation appends 1 Paragraph to the template document."
-    (let [output-file-path (docx/transform [{:type :append-text :replacement "appended text as a map"}])]
-      (sh "timeout" "5s" "libreoffice" "--norestore" output-file-path)))
-
-  (testing "Testing if transformation appends 1 image to the template document."
-    (let [output-file-path (docx/transform [{:type :append-image :replacement (.getPath (io/resource "test-image.jpg"))}])]
-      (sh "timeout" "5s" "libreoffice" "--norestore" output-file-path)))
-
-  (testing "Testing if transformation appends 1 table to the template document."
-    (let [output-file-path (docx/transform [{:type :append-table :replacement [["cell 11" "cal 12" "cell 13"] ["cell 21" "cell 22" "cell 23"]]}])]
-      (sh "timeout" "5s" "libreoffice" "--norestore" output-file-path)))
-
-  (testing "Testing if transformation appends 1 bullet list to the template document."
-    (let [output-file-path (docx/transform [{:type :append-bullet-list :replacement ["item a" "item b" "item c"]}])]
-      (sh "timeout" "5s" "libreoffice" "--norestore" output-file-path)))
-
-  (testing "Testing if transformation appends 1 numbered list to the template document."
-    (let [output-file-path (docx/transform [{:type :append-numbered-list :replacement ["item 1" "item 2" "item 3"]}])]
-      (sh "timeout" "5s" "libreoffice" "--norestore" output-file-path))))
+    (is (thrown? Exception (docx/transform "DOES-NOT-EXIST.docx" nil)))))
