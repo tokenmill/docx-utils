b6be829a40421b528567b6c41847271768a7726a
Dainius Jocas <dainius.jocas@tokenmill.lt>
2017-07-04 14:45:12 +0300

    details in the project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject docx-utils "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject lt.tokenmill/docx-utils "0.1.0-SNAPSHOT"
+  :description "Library to transform .docx documents."
+  :url "https://github.com/tokenmill/docx-utils"
+  :license {:name "MIT License"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.apache.xmlbeans/xmlbeans "2.6.0"]
