9602464fe86ff663d2faf1bdc4bc90c438a425e9
Dainius Jocas <dainius.jocas@tokenmill.lt>
2017-07-05 18:26:50 +0300

    version bump to “0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lt.tokenmill/docx-utils "0.1.0-SNAPSHOT"
+(defproject lt.tokenmill/docx-utils "0.1.0"
   :description "Library to transform .docx documents."
   :url "https://github.com/tokenmill/docx-utils"
   :license {:name "MIT License"}
